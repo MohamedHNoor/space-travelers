359b5502e906f6d3e4934949e82371c9d4e379da
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-01-30 22:36:31 +0200

    Dispatch the actions to the store

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,22 +1,61 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { reserveRocket, cancelRocket } from '../../redux/rockets/rockets';
 
-const Rocket = () => (
-  <div className="rocket-list">
-    <div className="image-frame">Image</div>
-    <div className="rocket-info">
-      <h3>Rocket Name</h3>
-      <div>
-        <span className="reserved-rocket">Reserved</span>
-        <span className="rocket-description">Rocket Description</span>
+const Rocket = ({ rocket }) => {
+  const {
+    name, description, imgs, id,
+  } = rocket;
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="rocket-list">
+      <div className="image-frame">
+        <img src={imgs[0]} alt="rockets" className="rocket-image" />
       </div>
-      <button type="button" className="reserveBtn">
-        Reserve Rocket
-      </button>
-      <button type="button" className="cancelBtn">
-        Cancel Reservation
-      </button>
+      <div className="rocket-info">
+        <h3>{name}</h3>
+        <div>
+          <p>
+            {rocket.reserved && (
+              <span className="reserved-rocket">Reserved</span>
+            )}
+            {description}
+          </p>
+        </div>
+        {!rocket.reserved && (
+          <button
+            className="reserveBtn"
+            type="button"
+            onClick={() => dispatch(reserveRocket(id))}
+          >
+            Reserve Rocket
+          </button>
+        )}
+        {rocket.reserved && (
+          <button
+            className="cancelBtn"
+            type="button"
+            onClick={() => dispatch(cancelRocket(id))}
+          >
+            Cancel Reservation
+          </button>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Rocket.propTypes = {
+  rocket: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.number,
+    description: PropTypes.string,
+    reserved: PropTypes.bool,
+    imgs: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
 
 export default Rocket;
