78c2a20d97ab43b517e0f2d1322975849cba27fc
codehass <hassan.elouardy06@gmail.com>
2023-01-31 17:41:47 +0100

    Add profil component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../../redux/mission/mission';
+import './Profile.css';
 
-const Profile = () => <div className="container">I am the profile page</div>;
+let render = true;
+
+const Profile = () => {
+  const missions = useSelector((state) => state.missionReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!render) {
+      return;
+    }
+    render = false;
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  const newArray = missions.filter((mission) => mission.joined === true);
+
+  const myProfile = (array) => {
+    let output = '';
+    if (array.length === 0) {
+      output = <div className="no-mission">No mission</div>;
+    } else {
+      output = array.map((mission) => (
+        <li key={mission.mission_id} className="mission-item">
+          {mission.mission_name}
+        </li>
+      ));
+    }
+
+    return output;
+  };
+
+  return (
+    <div className="profile-container">
+      <div className="mission-container">
+        <h2>My Missions</h2>
+        <ul>{myProfile(newArray)}</ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
