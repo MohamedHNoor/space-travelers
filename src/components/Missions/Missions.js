17aa99394da7f240d43c181bbda526f686e317c5
codehass <hassan.elouardy06@gmail.com>
2023-01-31 17:38:36 +0100

    Add misson component script

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Mission from './Mission';
+import './Missions.css';
+import { getMissions } from '../../redux/mission/mission';
 
-const Missions = () => <div className="container">I am the missions page</div>;
+let render = true;
 
+const Missions = () => {
+  const missions = useSelector((state) => state.missionReducer);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!render) {
+      return;
+    }
+    render = false;
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>.</th>
+            <td />
+          </tr>
+          {missions.map((mission) => (
+            <Mission
+              key={mission.mission_id}
+              id={mission.mission_id}
+              name={mission.mission_name}
+              description={mission.description}
+              joined={mission.joined}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 export default Missions;
